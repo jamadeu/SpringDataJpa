4cdc04cc2b504291eafe0491eb09c838a4783652
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-09 18:55:25 -0300

    CrudCargoService

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,13 +1,42 @@
 package br.com.alura.spring.data;
 
+import br.com.alura.spring.data.service.CrudCargoService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataApplication.class, args);
-	}
+    private Boolean system = true;
+    private final CrudCargoService cargoService;
 
+    public SpringDataApplication(CrudCargoService cargoService) {
+        this.cargoService = cargoService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (system) {
+            System.out.println("Qual acao voce quer executar");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Cargo");
+
+            int action = scanner.nextInt();
+            if (action == 1) {
+                cargoService.inicial(scanner);
+            } else {
+                system = false;
+            }
+
+        }
+    }
 }
