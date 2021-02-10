a9df3478fc7a8beaf44e106c0281e09c6c4f4b08
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-10 18:04:47 -0300

    Specification

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,9 +1,6 @@
 package br.com.alura.spring.data;
 
-import br.com.alura.spring.data.service.CrudCargoService;
-import br.com.alura.spring.data.service.CrudFuncionarioService;
-import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
-import br.com.alura.spring.data.service.RelatoriosService;
+import br.com.alura.spring.data.service.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,20 +13,22 @@ import java.util.Scanner;
 public class SpringDataApplication implements CommandLineRunner {
 
     private Boolean system = true;
-
     private final CrudCargoService cargoService;
+    private final RelatoriosService relatoriosService;
     private final CrudFuncionarioService funcionarioService;
     private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
-    private final RelatoriosService relatoriosService;
+    private final RelatorioFuncionarioDinamico relatorioFuncionarioDinamico;
 
     public SpringDataApplication(CrudCargoService cargoService,
+                                 RelatoriosService relatoriosService,
                                  CrudFuncionarioService funcionarioService,
                                  CrudUnidadeTrabalhoService unidadeTrabalhoService,
-                                 RelatoriosService relatoriosService) {
+                                 RelatorioFuncionarioDinamico relatorioFuncionarioDinamico) {
         this.cargoService = cargoService;
+        this.relatoriosService = relatoriosService;
         this.funcionarioService = funcionarioService;
         this.unidadeTrabalhoService = unidadeTrabalhoService;
-        this.relatoriosService = relatoriosService;
+        this.relatorioFuncionarioDinamico = relatorioFuncionarioDinamico;
     }
 
     public static void main(String[] args) {
@@ -43,13 +42,12 @@ public class SpringDataApplication implements CommandLineRunner {
         while (system) {
             System.out.println("Qual função deseja executar?");
             System.out.println("0 - Sair");
-            System.out.println("1 - Funcionario");
-            System.out.println("2 - Cargo");
+            System.out.println("1 - Cargo");
+            System.out.println("2 - Funcionario");
             System.out.println("3 - Unidade");
             System.out.println("4 - Relatorios");
-
+            System.out.println("5 - Relatorio dinamico");
             Integer function = scanner.nextInt();
-
             switch (function) {
                 case 1:
                     cargoService.inicial(scanner);
@@ -62,6 +60,9 @@ public class SpringDataApplication implements CommandLineRunner {
                     break;
                 case 4:
                     relatoriosService.inicial(scanner);
+                    break;
+                case 5:
+                    relatorioFuncionarioDinamico.inicial(scanner);
                     break;
                 default:
                     System.out.println("Finalizando");
