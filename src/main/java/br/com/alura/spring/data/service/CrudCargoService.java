be0623c0223d12b1953cd7eb5842e4378ce59a63
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-10 13:40:45 -0300

    Atualizar cargo

--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -9,21 +9,55 @@ import java.util.Scanner;
 @Service
 public class CrudCargoService {
     private final CargoRepository cargoRepository;
+    private boolean system = true;
+
 
     public CrudCargoService(CargoRepository cargoRepository) {
         this.cargoRepository = cargoRepository;
     }
 
-    public void inicial(Scanner scanner){
-        salvar(scanner);
+    public void inicial(Scanner scanner) {
+        while (system) {
+            System.out.println("Qual ação de cargos deseja executar");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Salvar");
+            System.out.println("2 - Atualizar");
+
+            int action = scanner.nextInt();
+
+            switch (action) {
+                case 1:
+                    salvar(scanner);
+                    break;
+                case 2:
+                    atualizar(scanner);
+                    break;
+                default:
+                    system = false;
+                    break;
+            }
+        }
+
     }
 
-    public void salvar(Scanner scanner){
+    public void salvar(Scanner scanner) {
         System.out.println("Descricao do cargo");
         String descricao = scanner.next();
         Cargo cargo = new Cargo();
         cargo.setDescricao(descricao);
         cargoRepository.save(cargo);
         System.out.println("Salvo");
+    }
+
+    public void atualizar(Scanner scanner) {
+        System.out.println("Id do cargo");
+        int id = scanner.nextInt();
+        System.out.println("Descricao do cargo");
+        String descricao = scanner.next();
+        Cargo cargo = new Cargo();
+        cargo.setDescricao(descricao);
+        cargo.setId(id);
+        cargoRepository.save(cargo);
+        System.out.println("Atualizado");
     }
 }
